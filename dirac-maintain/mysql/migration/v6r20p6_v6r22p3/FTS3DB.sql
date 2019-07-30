f5e14bd45d789e78e0f58ea9a4b2dd20a71c0a00
Xianghu Zhao <zhaoxh@ihep.ac.cn>
2019-07-30 12:52:28 +0800

    Fix migration bugs

--- dirac-maintain/mysql/migration/v6r20p6_v6r22p3/FTS3DB.sql
+++ dirac-maintain/mysql/migration/v6r20p6_v6r22p3/FTS3DB.sql
@@ -1,6 +1,6 @@
 USE FTS3DB;
 
 
-ALTER TABLE Operations ADD INDEX('rmsOpID');
+ALTER TABLE Operations ADD INDEX `ix_Operations_rmsOpID` (`rmsOpID`);
 
 ALTER TABLE `Files` ADD COLUMN `ftsGUID` varchar(255) DEFAULT NULL AFTER `error`;
